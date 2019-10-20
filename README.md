5feefc25941c054dd9b136ece06e4b737da9d04d
Anastasia <anastasia.kolevatykh@gmail.com>
2019-10-20 22:43:06 +0300

    Added program

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # isosceles-triangles
+
+java version: 12
+sdk: 12
+
+Запуск в командной строке с аргуметами: java src/com/company/IsoscelesTriangles.java <in.txt> <out.txt>
+
+Особенности реализации:
+1. Входной файл должен быть в UTF-8 (другие кодировки не тестировались)
+2. Выходной файл в UTF-8
+3. Программа тестировалась в среде MacOS
---
+++ src/com/company/IsoscelesTriangles.java
@@ -0,0 +1,98 @@
+package com.company;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class IsoscelesTriangles {
+    private static double curSquare;
+    private static double maxSquare;
+    private static String coordsMax = "";
+
+    private static int getPreLength(int a, int b) {
+        return (int) Math.pow( a, 2 ) + (int) Math.pow( b, 2 );
+    }
+
+    private static double getLineLength(int preLength) {
+        return Math.pow( preLength, 0.5 );
+    }
+
+    private static double calcSquare(double a, double b) {
+        return (b * (Math.pow( Math.pow( a, 2 ) - Math.pow( b, 2 ) / 4, 0.5 ))) / 2;
+    }
+
+    private static void evaluate(String statement) {
+        String[] arr = statement.split( " " );
+
+        if (arr.length != 6) {
+            return;
+        }
+
+        int[] res = new int[6];
+        for (int i = 0; i < 6; i++) {
+            try {
+                res[i] = Integer.valueOf( arr[i] );
+            } catch (Exception e) {
+                return;
+            }
+        }
+
+        // Не берем корень, чтобы были только целые числа для сравнения
+        int preLine1 = getPreLength( res[0] - res[1], res[2] - res[3] );
+        int preLine2 = getPreLength( res[0] - res[1], res[4] - res[5] );
+        int preLine3 = getPreLength( res[2] - res[3], res[4] - res[5] );
+
+        if (preLine1 == preLine2 || preLine1 == preLine3 || preLine2 == preLine3) {
+            double line1 = getLineLength( preLine1 );
+            double line2 = getLineLength( preLine2 );
+            double line3 = getLineLength( preLine3 );
+
+            if (preLine1 == preLine2) {
+                curSquare = calcSquare( line1, line3 );
+            } else if (preLine1 == preLine3) {
+                curSquare = calcSquare( line1, line2 );
+            } else {
+                curSquare = calcSquare( line2, line1 );
+            }
+
+            if (updateMaxSquare()) {
+                coordsMax = statement;
+            }
+        }
+    }
+
+    private static boolean updateMaxSquare() {
+        if (maxSquare < curSquare) {
+            maxSquare = curSquare;
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // check command line arguments
+        if (args.length != 2) {
+            System.out.println( "Wrong Arguments count. Usage: IsoscelesTriangles <in.txt> <out.txt>" );
+        }
+
+        // read the input file by lines
+        try (Stream<String> stream = Files.lines( Paths.get( args[0] ) )) {
+            stream.forEach( IsoscelesTriangles::evaluate );
+        } catch (IOException e) {
+            System.out.print( "Failed to read " + args[0]);
+            e.printStackTrace();
+        }
+
+        // write to output file
+        try {
+            PrintWriter writer = new PrintWriter( args[1], StandardCharsets.UTF_8 );
+            writer.println( coordsMax );
+            writer.close();
+        } catch (IOException e) {
+            System.out.print( "Failed to write " + args[1]);
+            e.printStackTrace();
+        }
+    }
+}
